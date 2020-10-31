8634e984d7ad99209249001602aabc853c69100e
vinicio <vinicioferreira@sonner.com.br>
2020-10-31 11:39:00 -0300

    Initial commit

---
+++ src/test/java/br/com/vinicio/person/PersonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicio.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
