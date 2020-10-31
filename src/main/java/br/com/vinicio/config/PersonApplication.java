c2d348c32b897d0c78b042a4710f2af5a1095f18
vinicio <vinicioferreira@sonner.com.br>
2020-10-31 15:22:34 -0300

    create service person

--- src/main/java/br/com/vinicio/config/PersonApplication.java
+++ src/main/java/br/com/vinicio/config/PersonApplication.java
@@ -2,8 +2,10 @@ package br.com.vinicio.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("br.com.vinicio")
 public class PersonApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/vinicio/resource/PersonResource.java
+++ src/main/java/br/com/vinicio/resource/PersonResource.java
@@ -1,24 +1,22 @@
 package br.com.vinicio.resource;
 
+import br.com.vinicio.dto.request.CreatePersonRequest;
+import br.com.vinicio.model.Person;
 import br.com.vinicio.sevice.PersonService;
-import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Api(value = "person")
+@RequestMapping("/person")
 public class PersonResource {
 
     @Autowired
     private PersonService personService;
 
-
-
-
-    @RequestMapping(value = "/custom", method = RequestMethod.POST)
-    public String custom() {
-        return "custom";
+    @PostMapping("/")
+    public Person createPerson(CreatePersonRequest createPersonRequest) {
+        return personService.createPerson(createPersonRequest);
     }
 }
